49a7b4e6fee280b3000ed646898279bb16707d2c
Gabriel Rojas <35940919+gabo2192@users.noreply.github.com>
2024-06-24 14:37:22 -0500

    feat: update contracts

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -1,10 +1,10 @@
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
 
-import { HardhatUserConfig, vars } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 
-const TESTNET_WALLET = vars.get("TESTNET_WALLET");
-const MAINNET_WALLET = vars.get("MAINNET_WALLET");
+// const TESTNET_WALLET = vars.get("TESTNET_WALLET");
+// const MAINNET_WALLET = vars.get("MAINNET_WALLET");
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.20",
@@ -17,16 +17,16 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: "../web/src/typechain-types",
   },
-  networks: {
-    rskTestnet: {
-      url: "https://public-node.testnet.rsk.co",
-      accounts: [TESTNET_WALLET],
-    },
-    rsk: {
-      url: "https://public-node.rsk.co",
-      accounts: [MAINNET_WALLET],
-    },
-  },
+  // networks: {
+  //   rskTestnet: {
+  //     url: "https://public-node.testnet.rsk.co",
+  //     accounts: [TESTNET_WALLET],
+  //   },
+  //   rsk: {
+  //     url: "https://public-node.rsk.co",
+  //     accounts: [MAINNET_WALLET],
+  //   },
+  // },
 };
 
 export default config;
