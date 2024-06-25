46de54c44c3c69eff908fe8955940ea77d4167a8
Gabriel Rojas <35940919+gabo2192@users.noreply.github.com>
2024-06-25 15:51:16 -0500

    hotfix: fix import tree

--- apps/web/src/app/claim/page.tsx
+++ apps/web/src/app/claim/page.tsx
@@ -1,9 +1,6 @@
 import { authOptions } from "@/lib/auth";
 import { supabase } from "@/lib/supabase";
-import {
-  generateMerkleTreeRoot2,
-  generateProof,
-} from "@/utils/generate-merkle-tree";
+import { generateProof } from "@/utils/generate-merkle-tree";
 import { Layout } from "@components/layout";
 import { getServerSession } from "next-auth";
 import { signOut } from "next-auth/react";
@@ -12,7 +9,6 @@ import { ClaimButton } from "./components/claim-button";
 
 export default async function CheckEligibility() {
   const session = await getServerSession(authOptions);
-  generateMerkleTreeRoot2();
   if (!session) {
     return redirect("/");
   }
--- apps/web/src/utils/generate-merkle-tree.ts
+++ apps/web/src/utils/generate-merkle-tree.ts
@@ -1,9 +1,8 @@
 import values from "@/data/accounts.json";
 import values2 from "@/data/accounts2.json";
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
-import * as fs from "fs";
+import data from "../../tree.json";
 
-import { getAddress } from "ethers";
 import { MerkleTree } from "./merkleTree";
 const vals = [...values, ...values2];
 
@@ -22,35 +21,33 @@ export const generateMerkleTreeProof = (address: string) => {
   return proof;
 };
 
-export const generateMerkleTreeRoot2 = () => {
-  const badAddresses = [];
-  const data = vals
-    .map((v) => {
-      try {
-        const address = getAddress(v.toLowerCase());
-        return [address, "100"];
-      } catch (error) {
-        if ((error as any).code === "INVALID_ARGUMENT") {
-          badAddresses.push(v);
-          return null;
-          // Handle the error appropriately (e.g., skip the address, log it, etc.)
-        } else {
-          console.log({ v });
-          return null;
-        }
-      }
-    })
-    .filter((v) => v !== null);
-  const tree = StandardMerkleTree.of(data, ["address", "uint"]);
+// export const generateMerkleTreeRoot2 = () => {
+//   const badAddresses = [];
+//   const data = vals
+//     .map((v) => {
+//       try {
+//         const address = getAddress(v.toLowerCase());
+//         return [address, "100"];
+//       } catch (error) {
+//         if ((error as any).code === "INVALID_ARGUMENT") {
+//           badAddresses.push(v);
+//           return null;
+//           // Handle the error appropriately (e.g., skip the address, log it, etc.)
+//         } else {
+//           console.log({ v });
+//           return null;
+//         }
+//       }
+//     })
+//     .filter((v) => v !== null);
+//   const tree = StandardMerkleTree.of(data, ["address", "uint"]);
 
-  console.log("Merkle Root:", tree.root);
-  fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
-};
+//   console.log("Merkle Root:", tree.root);
+//   fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
+// };
 
 export const generateProof = (address: string) => {
-  const tree = StandardMerkleTree.load(
-    JSON.parse(fs.readFileSync("tree.json", "utf8"))
-  );
+  const tree = StandardMerkleTree.load(data as any);
   for (const [i, v] of tree.entries()) {
     if (v[0] === address) {
       // (3)
