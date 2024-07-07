595811c95d5216bdc856a51494b27ad0f318ba0e
Gabriel Rojas <35940919+gabo2192@users.noreply.github.com>
2024-07-07 18:55:00 -0500

    feat: update home page layout

--- apps/web/src/app/(token-airdrop)/page.tsx
+++ apps/web/src/app/(token-airdrop)/page.tsx
@@ -1,13 +1,9 @@
-import { XLogo } from "@/assets/twitter-logo";
 import { CheckEligibility } from "@/components/client/check-eligibility";
 import { ConnectWallet } from "@/components/client/connect-wallet";
-import { MainLayout } from "@/components/layouts/main-layout";
-import { Hero } from "@/components/sections/hero";
 
 export default function Page() {
   return (
-    <MainLayout className=" flex flex-col justify-center">
-      <Hero />
+    <>
       <h2 className="font-koulen text-5xl text-center mb-4 max-w-lg mx-auto">
         We're creating a mem ecosystem on{" "}
         <span className="text-accentBrand">Rootstock</span>
@@ -20,14 +16,6 @@ export default function Page() {
         <ConnectWallet />
         <CheckEligibility />
       </div>
-      <div className=" md:mt-auto">
-        <div className="text-base font-medium items-center flex flex-col gap-2">
-          Contact
-          <a href="https://x.com/madgeymooo" target="_blank" rel="noreferrer">
-            <XLogo className="size-10" />
-          </a>
-        </div>
-      </div>
-    </MainLayout>
+    </>
   );
 }
